bc2cbb85695a425bab25b3fbb67a19760fb5d7c9
cassidyjean9 <51214010+cassidyjean9@users.noreply.github.com>
2020-12-03 19:36:12 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  //make sure jquery is working
+  console.log("working");
+
+  //time delay for animation
+  setTimeout(function () {
+      $(".work").css("display", "block");
+}, 3000);
+});
